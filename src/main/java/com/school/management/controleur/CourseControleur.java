ea9ced53821051be9665f160eb199542bbb54c4e
naoufal <naoufal.dev@gmail.com>
2021-08-08 14:50:26 +0100

    add input date and image also show them in table

--- src/main/java/com/school/management/controleur/CourseControleur.java
+++ src/main/java/com/school/management/controleur/CourseControleur.java
@@ -1,15 +1,19 @@
 package com.school.management.controleur;
 
+import java.beans.Transient;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.school.management.entities.Courses;
+import com.school.management.helper.FileUploadUtil;
 import com.school.management.services.CoursesServices;
 
 @Controller
@@ -28,12 +32,29 @@ public class CourseControleur {
 	}
 
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public String save(Courses p, Model model, @RequestParam(name = "code", defaultValue = "-1") Long code)
-			throws IOException {
+	public String save(Courses p, Model model, @RequestParam(name = "code", defaultValue = "-1") Long code,
+			@RequestParam("image") MultipartFile multipartFile) throws IOException {
+		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+		p.setPhotos(fileName);
 
 		coursesServices.addCources(p, code);
 
+		String uploadDir = "courses-photos/" + p.getIdCourses();
+
+		FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+
 		return "redirect:/adminCourseForm";
 	}
-
+	@RequestMapping(value = "/admintablecourse")
+	public String admintablecourse(Model model) {
+		model.addAttribute("courses", coursesServices.findAllCourses());
+		return "table-courses";
+	}
+//	@Transient
+//	public String getPhotosImagePath() {
+//		if (photos == null || id == null)
+//			return null;
+//
+//		return "/courses-photos/" + id + "/" + photos;
+//	}
 }
--- src/main/java/com/school/management/entities/Courses.java
+++ src/main/java/com/school/management/entities/Courses.java
@@ -1,13 +1,19 @@
 package com.school.management.entities;
 
+import java.beans.Transient;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Courses {
@@ -18,32 +24,57 @@ public class Courses {
 	private String courseTitle;
 	@Lob
 	private String courseDescription;
-//	private String courseType;
+
 	private String courseVideoID;
 	private String courseDriveLink;
-//	private Date coursePublish;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Temporal(TemporalType.DATE)
+	private Date coursePublish;
+
 	@ManyToOne
 	@JoinColumn(name = "code_category")
 	private Category category;
+
+	@Column(nullable = true, length = 64)
+	private String photos;
 
 	public Courses() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	 
-
 	public Courses(String courseTitle, String courseDescription, String courseVideoID, String courseDriveLink,
-			Category category) {
+			Date coursePublish, Category category, String photos) {
 		super();
 		this.courseTitle = courseTitle;
 		this.courseDescription = courseDescription;
 		this.courseVideoID = courseVideoID;
 		this.courseDriveLink = courseDriveLink;
+		this.coursePublish = coursePublish;
 		this.category = category;
+		this.photos = photos;
+	}
+	@Transient
+    public String getPhotosImagePath() {
+        if (photos == null || this.idCourses == null) return null;
+         
+        return "/courses-photos/" + this.idCourses + "/" + photos;
+    }
+	public String getPhotos() {
+		return photos;
 	}
 
+	public void setPhotos(String photos) {
+		this.photos = photos;
+	}
 
+	public Date getCoursePublish() {
+		return coursePublish;
+	}
+
+	public void setCoursePublish(Date coursePublish) {
+		this.coursePublish = coursePublish;
+	}
 
 	public String getCourseTitle() {
 		return courseTitle;
@@ -69,8 +100,6 @@ public class Courses {
 		this.courseDescription = courseDescription;
 	}
 
-	 
-
 	public String getCourseVideoID() {
 		return courseVideoID;
 	}
@@ -87,7 +116,6 @@ public class Courses {
 		this.courseDriveLink = courseDriveLink;
 	}
 
-	 
 	public Category getCategory() {
 		return category;
 	}
